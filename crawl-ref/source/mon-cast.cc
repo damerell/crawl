7895b1d62fa49091cc8c70799fcb9f73fc6a42ce
Timothy Stiles <Patashu@hotmail.com>
2018-03-13 15:49:10 +1100

    Orbageddon (Lv9 Conj) - initial commit
    Lv9 Conj
    
    Fires Orbs of Destruction in all 8 directions. In addition to homing in on
    their target, they will explode (as per Iskenderun's Mystic Blast) when they
    hit something. The Orbs can hit and damage you, but the explosions will not.
    
    (cherry picked from commit 1966cbe8514163ac281c94734476f3b4dcde5a0a)

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -1175,6 +1175,7 @@ static int _mons_power_hd_factor(spell_type spell, bool random)
         case SPELL_SPECTRAL_WEAPON:
         case SPELL_IGNITE_POISON:
         case SPELL_IOOD:
+        case SPELL_ORBAGEDDON:
             return 6;
 
         case SPELL_SUMMON_DRAGON:
