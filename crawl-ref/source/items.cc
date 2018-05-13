b059e39deb893b3b6d4a294a38a37a318f2907f1
David Damerell <damerell@chiark.greenend.org.uk>
2018-05-13 18:24:01 +0100

    Revert "Use random food tiles for rations" unnecessary without boring food
    This reverts commit af8bc5dced2e40a49988ad03a78819f93b343fa2.

--- crawl-ref/source/items.cc
+++ crawl-ref/source/items.cc
@@ -80,7 +80,6 @@
 #include "stringutil.h"
 #include "terrain.h"
 #include "throw.h"
-#include "tilepick.h"
 #include "travel.h"
 #include "unwind.h"
 #include "viewchar.h"
@@ -2067,13 +2066,6 @@ static int _place_item_in_free_slot(item_def &it, int quant_got,
     {
         set_ident_flags(item, ISFLAG_IDENT_MASK);
         mark_had_book(item);
-    }
-
-    // Normalize ration tile in inventory
-    if (item.base_type == OBJ_FOOD && item.sub_type == FOOD_RATION)
-    {
-        item.props["item_tile_name"] = "food_ration_inventory";
-        bind_item_tile(item);
     }
 
     note_inscribe_item(item);
