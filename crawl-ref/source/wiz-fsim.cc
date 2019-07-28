damerell/crawl

commit 32c3287dfddcedcf3fbcc03f87949220fdbd70cb

Add Hep to, and better place Uk in, zigsprint.
Expand the starting room by two squares vertically, reducing rooms 22
and 23 to make it fit.

crawl-ref/source/dat/des/sprint/zigsprint.des
@@ -627,9 +627,9 @@ xxAAAAAAAAAAAAAAAxxUUUUUUUUUUUUUUUxRRRRRRRRRRRRRxPPPPPPPPPPPxxxxxxxxxxxxxxxxxxxx
 xxAAAAAAAAAAAAAAAxxUUUUUUUUUUUUUUUxRRRRRRRRRRRRRxPPPPPPPPPPPxEEEEEEEExFFFFFFFFxx
 xxAAAAAAAAAAAAAAAxxUUUUUUUUUUUUUUUxRRRRRRRRRRRRRxPPPPPPPPPPPxEEEEEEEExFFFFFFFFxx
 xxAAAAAAAAAAAAAAAxxUUUUUUUUUUUUUUUxRRRRRRRRRRRRRxPPPPPPPPPPPxEEEEEEEExFFFFFFFFxx
-xxxxxxxxxxxxxxxxxxxUUUUUUUUUUUUUUUxRRRRRRRRRRRRRxxxxxxxxxxxxxEEEEEEEExFFFFFFFFxx
-xYYYYYYYYYYYYYYYYYxUUUUUUUUUUUUUUUxRRRRRRRRRRRRRxOOOOOOOOOOOxEEEEEEEExFFFFFFFFxx
-xYYYYYYYYYYYYYYYYYxUUUUUUUUUUUUUUUxxxxxxxxxxxxxxxOOOOOOOOOOOxEEEEEEEExFFFFFFFFxx
+xxAAAAAAAAAAAAAAAxxUUUUUUUUUUUUUUUxRRRRRRRRRRRRRxxxxxxxxxxxxxEEEEEEEExFFFFFFFFxx
+xxAAAAAAAAAAAAAAAxxUUUUUUUUUUUUUUUxRRRRRRRRRRRRRxOOOOOOOOOOOxEEEEEEEExFFFFFFFFxx
+xxxxxxxxxxxxxxxxxxxUUUUUUUUUUUUUUUxxxxxxxxxxxxxxxOOOOOOOOOOOxEEEEEEEExFFFFFFFFxx
 xYYYYYYYYYYYYYYYYYxUUUUUUUUUUUUUUUxSSSSSSSSSSSSSxOOOOOOOOOOOxEEEEEEEExFFFFFFFFxx
 xYYYYYYYYYYYYYYYYYxxxxxxxxxxxxxxxxxSSSSSSSSSSSSSxOOOOOOOOOOOxxxxxxxxxxxxxxxxxxxx
 xYYYYYYYYYYYYYYYYYxVVVVVVVVVVVVVVVxSSSSSSSSSSSSSxOOOOOOOOOOOxIIIIIIIIIxGGGGGGGxx
@@ -646,8 +646,8 @@ xYYYYYYYYYYYYYYYYYxVVVVVVVVVVVVVVVxSSSSSSSSSSSSSxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
 xYYYYYYYYYYYYYYYYYxVVVVVVVVVVVVVVVxxxxxxxxxxxxxxxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
 xYYYYYYYYYYYYYYYYYxVVVVVVVVVVVVVVVxTTTTTTTTTTTTTxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
 xYYYYYYYYYYYYYYYYYxVVVVVVVVVVVVVVVxTTTTTTTTTTTTTxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
-xxxxxxxxxxxxxxxxxxxVVVVVVVVVVVVVVVxTTTTTTTTTTTTTxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
-xZZZZZZZZZZZZZZZZZxxxxxxxxxxxxxxxxxTTTTTTTTTTTTTxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
+xYYYYYYYYYYYYYYYYYxVVVVVVVVVVVVVVVxTTTTTTTTTTTTTxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxTTTTTTTTTTTTTxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
 xZZZZZZZZZZZZZZZZZxWWWWWWWWWWWWWWWxTTTTTTTTTTTTTxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
 xZZZZZZZZZZZZZZZZZxWWWWWWWWWWWWWWWxTTTTTTTTTTTTTxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
 xZZZZZZZZZZZZZZZZZxWWWWWWWWWWWWWWWxTTTTTTTTTTTTTxNNNNNNNNNNNxJJJJJJJJJxHHHHHHHxx
@@ -714,6 +714,7 @@ KFEAT:  u = altar_qazlal
 KFEAT:  v = altar_ru
 KFEAT:  w = altar_pakellas
 KFEAT:  x = altar_ukayaw
+KFEAT:  y = altar_hepliaklqana
 KITEM:    B = plate armour mundane not_cursed ident:all plus:5, \
               fire dragon armour mundane not_cursed ident:all plus:5, \
               ice dragon armour mundane not_cursed ident:all plus:5, \
@@ -804,17 +805,19 @@ KFEAT:    ^ = granite_statue
 KFEAT:    & = granite_statue
 COLOUR:   & = magenta
 MAP
-^f^g^h&A&i^j^k^
-e.........RT..l
-^..HG.........^
-d..IF.........m
-^..JE.....S...^
-c...D..M..P...n
-^......L..O...^
-b...C..K..N...o
-^...B.{.......^
-ax............p
-^w^v^u^t^s^r^q^
+^g^h^i&A&j^k^l^
+f.............m
+^.........RT..^
+e..HG.........n
+^..IF.........^
+d..JE.....S...o
+^...D..M..P...^
+c......L..O...p
+^...C..K..N...^
+b...B.{.......q
+^.............^
+a.............r
+^y^x^w^v^u^t^s^
 ENDMAP
 
 NAME:  room_1
@@ -1154,7 +1157,6 @@ MAP
 .................
 .................
 .................
-.................
 ...z.........z...
 ..zZz.......zZz..
 ...z.........z...
@@ -1169,7 +1171,6 @@ MAP
 ..1111111111111..
 ..1111111111111..
 ..1111111111111..
-.................
 .................
 .................
 .................

commit 132481a595fac26dc6b3ae24851b6799c4f776d6

Zigsprint and fsim fixen
In zigsprint, it is possible for the top row of superb items to get
one of the named items intended to be in the starting room. This is
easily fixed by moving the stacks of named items one space north.
This is dependent on the ordering that the .des file is processed
but we boldly assume this won't change.

Vanilla fixed this in commit
6933cff4f19b28b15837163d13262d717d8818e2
but then inadvertently broke it again in commit
32c3287dfddcedcf3fbcc03f87949220fdbd70cb

fsim's feature to vary your skill didn't know about magical staves.
Fixing this isn't a big improvement because the damage they do varies
largely with Evo and spell school, but the pre-fix situation where
it varies Unarmed Combat was clearly bogus.

crawl-ref/source/dat/des/sprint/zigsprint.des
@@ -813,8 +813,8 @@ KFEAT:    & = granite_statue
 COLOUR:   & = magenta
 MAP
 ^g^h^j&A&k^l^m^
-f.............n
-^.........RT..^
+f.........RT..n
+^.............^
 e..HG.........o
 ^..IF.........^
 d..JE.....S...q

crawl-ref/source/wiz-fsim.cc
@@ -86,8 +86,10 @@ static skill_type _equipped_skill()
     const int weapon = you.equip[EQ_WEAPON];
     const item_def * iweap = weapon != -1 ? &you.inv[weapon] : nullptr;
 
-    if (iweap && iweap->base_type == OBJ_WEAPONS)
+    if (iweap && 
+        (iweap->base_type == OBJ_WEAPONS || iweap->base_type == OBJ_STAVES)) {
         return item_attack_skill(*iweap);
+    }
 
     if (!iweap && you.m_quiver.get_fire_item() != -1)
         return SK_THROWING;