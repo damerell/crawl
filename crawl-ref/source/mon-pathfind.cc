59342873e202e5b40c9aff34060677432c4e0639
Aidan Holm <aidanholm@gmail.com>
2019-10-02 02:55:26 +0800

    Fix clinging monsters breaking autoexplore (shalmezad, 11924)
    Clinging monsters in vaults could pathfind their way through the runed
    door, and were therefore marked as not safe, preventing autoexplore.
    This was caused by mons_traversable() sidestepping checks for runed
    cells and uninhabitable cells in mons_can_traverse() if the monster can
    cling. I've just hoisted them out here.
    
    In addition, cell_can_cling_to() only checks physical location,
    and so couldn't check whether a feature is passable by a given monster.
    
    Last but not least, the call to cell_is_clingable() previously passed
    the wrong parameter.
    
    (cherry picked from commit 323ad4e9fb1dbb588b479de76b9c62a829cc0646)
    (cherry picked from commit 5cf9a61fe5651d87be5349c8388c5a59e248e7f0)
    (kinda)
    
    Amended in stoat soup to avoid, I think, the bug fixed in the latter
    commit.
    
    Checking first if the cell is runed is correct. No monster can pass a
    runed door.
    
    can_traverse will return true if it's an openable door. Hence by the time
    we get to is_habitable, we know it won't be being rejected by virtue of
    being a closed door.
    
    Then we check if we could cling through it and if not we return false

--- crawl-ref/source/mon-pathfind.cc
+++ crawl-ref/source/mon-pathfind.cc
@@ -445,10 +445,20 @@ bool monster_pathfind::traversable(const coord_def& p)
 // its preferred habit and capability of flight or opening doors.
 bool monster_pathfind::mons_traversable(const coord_def& p)
 {
-    return mons_can_traverse(*mons, p, traverse_in_sight)
-            || mons->can_cling_to_walls()
-               && cell_is_clingable(pos)
-               && cell_can_cling_to(pos, p);
+    if (cell_is_runed(p))
+        return false;
+    
+    if (mons_can_traverse(*mons, p, traverse_in_sight)) return true;
+    
+    if (!mons->is_habitable(p))
+        return false;
+
+    if (mons->can_cling_to_walls()
+        && cell_is_clingable(p)
+        && mons->can_pass_through_feat(grd(p))
+        && cell_can_cling_to(pos, p)) return true;
+
+    return false;
 }
 
 int monster_pathfind::travel_cost(coord_def npos)
