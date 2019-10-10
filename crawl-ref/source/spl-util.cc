aa601d3fc987191a11d12cadfc90227389cbb2d5
David Damerell <damerell@chiark.greenend.org.uk>
2019-10-10 22:48:55 +0100

    Formicids cannot cast Swiftness
    This has been a long-standing bone of contention in vanilla (see Mantis
    issue 7786) because the original formicid restrictions were a somewhat
    arbitary duplication of the effects of the now-gone amulet of stasis.
    
    After a short discussion we were 2/3 in favour of removal.
    
    This does not prevent swiftness from other sources but AFAICT at the
    moment there aren't any.

--- crawl-ref/source/spl-util.cc
+++ crawl-ref/source/spl-util.cc
@@ -1124,6 +1124,9 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
         break;
 
     case SPELL_SWIFTNESS:
+        if (you.species == SP_FORMICID)
+            return "your stasis precludes magical swiftness.";
+
         if (temp)
         {
             if (you.duration[DUR_SWIFTNESS])
