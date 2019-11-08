de4230fdce95db0217a9bf3e271b2ce506cbe00e
David Damerell <damerell@chiark.greenend.org.uk>
2019-11-08 21:49:35 +0000

    Detail recent change, normalise changelog format (oops)

--- crawl-ref/source/main.cc
+++ crawl-ref/source/main.cc
@@ -448,7 +448,7 @@ NORETURN static void _launch_game()
     if (!crawl_state.game_is_tutorial()) {
         mpr("Press <w>?</w> for a list of commands and other information.");
         if (game_start) {
-            mpr("See http://crawl.montres.org.uk/about.html for Stoat Soup changes.\nRecent changes: All magical staves do spell skill based damage in melee.");
+            mpr("See http://crawl.montres.org.uk/about.html for Stoat Soup changes.\nRecent changes: Restore High Council, Cekugob, Doom Knight, Eternal Torment.");
         }
     }
 
