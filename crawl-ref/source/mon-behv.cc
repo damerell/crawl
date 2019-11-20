edfe4a7372acf95f0712c5925a158949cbb70a93
advil <rawlins@gmail.com>
2019-11-19 21:45:25 -0500

    Various refactoring to make expensive calls less likely
    This is all in code paths that are involved in monster movement, so code
    that is called very frequently. Calls that check enchantments in
    particular seem to be disproportionately bad.

--- crawl-ref/source/mon-behv.cc
+++ crawl-ref/source/mon-behv.cc
@@ -940,15 +940,15 @@ static bool _mons_check_foe(monster* mon, const coord_def& p,
 
     monster* foe = monster_at(p);
     return foe && foe != mon
-           && (mon->has_ench(ENCH_INSANE)
-               || foe->friendly() != friendly
-               || neutral && !foe->neutral())
+           && (foe->friendly() != friendly
+               || neutral && !foe->neutral()
+               || mon->has_ench(ENCH_INSANE))
            && (ignore_sight || mon->can_see(*foe))
            && !foe->is_projectile()
            && summon_can_attack(mon, p)
            && (friendly || !is_sanctuary(p))
            && !mons_is_firewood(*foe)
-           || mon->has_ench(ENCH_INSANE) && p == you.pos();
+           || p == you.pos() && mon->has_ench(ENCH_INSANE);
 }
 
 // Choose random nearest monster as a foe.
