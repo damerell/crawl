9ecd64da6320a3f34d27067ecb89a5ba363326c8
sdynet <sdynet76@gmail.com>
2019-12-22 17:10:30 +0900

    Improve the Cloud card
    The old cloud card produces clouds that damage the enemy. This effect is
    more suitable for deck of destruction. The new effect creates black
    smoke that blocks vision instead of damaging enemy.
    
    This effect differs from fog scrolls and the tomb card by placing the
    clouds at the monsters, not surrounding the player in protection.
    
    [ Committer's note: revised the commit message, and included the
      monster's cell for cloud placement. Closes #1237 ]
    
    (cherry picked from commit 31946683ed5e7c890adf0bb2e14208c7079ba74e)

--- crawl-ref/source/decks.cc
+++ crawl-ref/source/decks.cc
@@ -2387,35 +2387,16 @@ static void _cloud_card(int power, deck_rarity_type rarity)
     {
         monster *mons = monster_at(*di);
         cloud_type cloudy;
-
-        switch (power_level)
-        {
-            case 0: cloudy = !one_chance_in(5) ? CLOUD_MEPHITIC : CLOUD_POISON;
-                    break;
-
-            case 1: cloudy = (you_worship(GOD_DITHMENOS) || coinflip())
-                              ? CLOUD_COLD : CLOUD_FIRE;
-                    break;
-
-            case 2: cloudy = (is_good_god(you.religion) || coinflip())
-                              ? CLOUD_ACID: CLOUD_MIASMA;
-                    break;
-
-            default: cloudy = CLOUD_DEBUGGING;
-        }
+        cloudy = CLOUD_BLACK_SMOKE;
 
         if (!mons || mons->wont_attack() || !mons_is_threatening(*mons))
             continue;
 
-        for (adjacent_iterator ai(mons->pos()); ai; ++ai)
+        for (adjacent_iterator ai(mons->pos(), false); ai; ++ai)
         {
-            // don't place clouds on the player or monsters
-            if (*ai == you.pos() || monster_at(*ai))
-                continue;
-
             if (grd(*ai) == DNGN_FLOOR && !cloud_at(*ai))
             {
-                const int cloud_power = 5 + random2((power_level + 1) * 3);
+                const int cloud_power = 5 + random2avg(power_level * 6, 2);
                 place_cloud(cloudy, *ai, cloud_power, &you);
 
                 if (you.see_cell(*ai))
