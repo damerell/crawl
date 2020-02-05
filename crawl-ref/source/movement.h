daea9d3735fbf7006bb34c30d15662b677f23525
David Damerell <damerell@chiark.greenend.org.uk>
2020-02-05 22:01:56 +0000

    Move move actions out of main.cc
    This resolves a longstanding TODO in main.cc.
    
    [Committer's note: fixed whitespace, edited commit message. Closes #856]
    
    This corresponds to vanilla commit c0b0c76f0acc73a49587e04f2ccd7df981282286
    but needed some wrangling owing to WJC and Ozo's Armour changes.

--- crawl-ref/source/movement.h
+++ crawl-ref/source/movement.h
@@ -1,9 +1,14 @@
 /**
  * @file
- * @brief Functions related to player movement
+ * @brief Movement, open-close door commands, movement effects.
 **/
 
 #pragma once
 
 bool cancel_barbed_move();
 void apply_barbs_damage();
+bool cancel_confused_move(bool stationary);
+void open_door_action(coord_def move = {0,0});
+void close_door_action(coord_def move);
+bool prompt_dangerous_portal(dungeon_feature_type ftype);
+void move_player_action(coord_def move);
