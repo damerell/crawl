5b2618dd03cd2f33b851fb0302d7556964592db3
Aidan Holm <aidanholm@gmail.com>
2020-04-13 23:50:04 +0800

    Fix bugged acquirement menu (#12234)
    It looks like select() can do a number of things, including toggling,
    but was in this instance a no-op. I've switched it to deselect_all() for
    the sake of readability.
    
    (cherry picked from commit 53dc3366b14c17ab610dc6898f6db08606f7b95f)

--- crawl-ref/source/acquire.cc
+++ crawl-ref/source/acquire.cc
@@ -1668,7 +1668,7 @@ bool AcquireMenu::acquire_selected()
 
     if (!yesno(nullptr, true, 'n', false, false, true))
     {
-        entry.select();
+        deselect_all();
         more = old_more;
         update_more();
         return true;
