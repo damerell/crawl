9ccf7bd3bdcaf3a1324fc51cbe06d03bf3e8853b
Edgar A. Bering IV <trizor@gmail.com>
2020-05-03 14:30:56 -0400

    Prevent Eldritch Tentacles from attacking out of LOS
    The summon-los-restriction dates to a9662255 in 2012. The commit message
    seems like this is intended to apply to all summons; however, eldritch
    tentacles are created with an abjuration duration of 0, so the code
    introduced at that time did not apply the restriction to them. Four
    years later, in 2016, the restriction was added to FAKE_ABJRATION
    allies. This too did not contrain the tentacle, which is timed out with
    ENCH_PORTAL_PACIFIED.
    
    In the 8 years since a9662255 this oversight or bug has become an
    accepted quirk of the spell and not treated like a bug. It is also a
    source of out-of-los attacks of the kind we intentionally prevent. So,
    in a long overdue commit this finally constrains malign gateways to not
    attacking out of LOS.
    
    (cherry picked from commit 27cb841e2324187bb4ebfc82b69afa67edd7876c)

--- crawl-ref/source/mon-behv.cc
+++ crawl-ref/source/mon-behv.cc
@@ -1512,6 +1512,7 @@ bool summon_can_attack(const monster* mons)
            || !mons->friendly()
            || !mons->is_summoned()
               && !mons->has_ench(ENCH_FAKE_ABJURATION)
+              && !mons->has_ench(ENCH_PORTAL_PACIFIED)
               && !mons_is_hepliaklqana_ancestor(mons->type)
            || you.see_cell_no_trans(mons->pos());
 }
@@ -1537,7 +1538,8 @@ bool summon_can_attack(const monster* mons, const coord_def &p)
     if (!mons->friendly()
         || !mons->is_summoned()
             && !mons->has_ench(ENCH_FAKE_ABJURATION)
-            && !mons_is_hepliaklqana_ancestor(mons->type))
+            && !mons_is_hepliaklqana_ancestor(mons->type)
+            && !mons->has_ench(ENCH_PORTAL_PACIFIED))
     {
         return true;
     }
