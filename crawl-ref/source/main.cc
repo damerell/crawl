98c6be209049f2b36a3dd4ea667205745d712c63
David Damerell <damerell@chiark.greenend.org.uk>
2020-08-13 15:11:27 +0100

    Acquirement timeout
    An uncashed acquirement times out after 500 turns, preventing HOM
    from, say, saving it forever until they see the Orc:2 shops.

--- crawl-ref/source/main.cc
+++ crawl-ref/source/main.cc
@@ -439,7 +439,7 @@ NORETURN static void _launch_game()
     if (!crawl_state.game_is_tutorial()) {
         mpr("Press <w>?</w> for a list of commands and other information.");
         if (game_start) {
-            mpr("See http://crawl.montres.org.uk/about-dev.html for changes.\nRecent change: slightly reworked 0.24 Throwing reforms.");
+            mpr("See http://crawl.montres.org.uk/about-dev.html for changes.\nRecent change: slightly reworked 0.25 acquirement.");
         }
     }
 
