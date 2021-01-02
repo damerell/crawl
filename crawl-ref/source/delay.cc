damerell/crawl

commit d51f7d1946ba6a110a4301f94d30cbd4423617db

Monster info pane reports on distortion
This required me to add this extra update_monster_pane after
mark_mon_equipment_seen because otherwise when a monster with a
weapon of disto is (eg) summoned in your field of view, things
happen in the wrong order. I have my doubts about this having
unwanted side effects...

crawl-ref/source/view.cc
@@ -557,6 +557,10 @@ void mark_mon_equipment_seen(const monster *mons)
                 item.flags |= ISFLAG_KNOW_TYPE;
         }
     }
+    // This causes summoned monsters with distortion weapons to show up in
+    // in the monster info list. Hopefully it doesn't have any harmful side
+    // effects.
+    update_monster_pane();
 }
 
 

crawl-ref/source/mon-info.cc
@@ -1935,6 +1935,14 @@ vector<monster_info_func> init_monster_info_funcs() {
     toret.push_back({"pinned", "pinned", 
                 [](const monster_info &mi, bool newconditions) {
                 return mi.is(MB_PINNED); }});
+    toret.push_back({"distortion", "distortion", 
+                [](const monster_info &mi, bool newconditions) {
+                if (mi.itemuse() >= MONUSE_STARTING_EQUIPMENT) {
+                    const item_def* weapon = mi.inv[MSLOT_WEAPON].get();
+                    if (weapon && 
+                        get_weapon_brand(*weapon) == SPWPN_DISTORTION)
+                        return newconditions; }
+                return false; }});
     toret.push_back({"polearm","polearms", 
                 [](const monster_info &mi, bool newconditions) {
                 if (mi.itemuse() >= MONUSE_STARTING_EQUIPMENT) {

commit 22790fdbf9382f00ef0cb4dcb9525b7a0e83589b

Fix monster distortion in pane
Commit d51f7d1946ba6a110a4301f94d30cbd4423617db caused objstats to
crash. This fixes the issue in a different way, by ensuring the equipment
on a summon is seen even if there is no message printed.

crawl-ref/source/delay.cc
@@ -1107,9 +1107,13 @@ static inline bool _monster_warning(activity_interrupt_type ai,
     if (!you.can_see(*mon))
         return false;
 
-    // Disable message for summons.
-    if (mon->is_summoned() && !delay)
+    // Disable message for summons - but do mark their gear seen so disto will
+    // show up immediately in the monster pane
+    if (mon->is_summoned() && !delay) {
+        mark_mon_equipment_seen(mon);
+        update_monster_pane();
         return false;
+    }
 
     if (at.context == SC_ALREADY_SEEN || at.context == SC_UNCHARM)
     {

crawl-ref/source/view.cc
@@ -553,10 +553,6 @@ void mark_mon_equipment_seen(const monster *mons)
                 item.flags |= ISFLAG_KNOW_TYPE;
         }
     }
-    // This causes summoned monsters with distortion weapons to show up in
-    // in the monster info list. Hopefully it doesn't have any harmful side
-    // effects.
-    update_monster_pane();
 }
 
 