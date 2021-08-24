b02953b4028fcd4fddc8e9963304f9374e6544cd
Kate <601195+semitonal@users.noreply.github.com>
2021-08-24 03:09:13 +0100

    Make Shadow Creatures a monster-only spell
    It doesn't work well with the reduced player summon caps, since a big part
    of its functionality is being able to create monster bands, and the
    interaction between bands and the summon cap was already fairly messy.
    Being able to summon rare branch-specific monsters and big monster bands is
    a fun effect, but works much better on the already-existent scroll of
    summoning, which avoids the problems of summon caps and is balanced by
    being a consumable.
    
    (cherry picked from commit 1a59d63638aabf693730afb1e7bc7f6fd725a041)

--- crawl-ref/source/spl-data.h
+++ crawl-ref/source/spl-data.h
@@ -1434,7 +1434,7 @@ static const struct spell_desc spelldata[] =
 {
     SPELL_SHADOW_CREATURES, "Shadow Creatures",
     spschool::summoning,
-    spflag::mons_abjure,
+    spflag::mons_abjure | spflag::monster,
     6,
     0,
     -1, -1,
