e85ee352fbea3469628376239eecf3694e072cda
David Damerell <damerell@chiark.greenend.org.uk>
2022-02-03 12:09:53 +0000

    Quokkas are silent, but weasels squeak
    So do rats, now monsters can squeak.
    
    Also move weasel description to the right place.

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -1368,6 +1368,7 @@ int get_shout_noise_level(const shout_type shout)
     case S_HISS:
     case S_VERY_SOFT:
         return 4;
+    case S_SQUEAK:
     case S_SOFT:
         return 6;
     case S_LOUD:
@@ -4685,6 +4686,7 @@ string do_mon_str_replacements(const string &in_msg, const monster& mons,
         "says",         // S_CHERUB -- they just speak normally.
         "squeals",
         "roars",
+        "squeaks",
         "buggily says", // NUM_SHOUTS
         "breathes",     // S_VERY_SOFT
         "whispers",     // S_SOFT
