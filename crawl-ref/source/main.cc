13af89c426c7f9fd71d6bdc8b87af08c6c0c6347
David Damerell <damerell@chiark.greenend.org.uk>
2022-02-03 01:59:13 +0000

    Rot heals with XP
    Partly because I hate being down one lousy max HP, and partly
    because we've restored AF_ROT to ugly things.

--- crawl-ref/source/main.cc
+++ crawl-ref/source/main.cc
@@ -439,7 +439,7 @@ NORETURN static void _launch_game()
     if (!crawl_state.game_is_tutorial()) {
         mpr("Press <w>?</w> for a list of commands and other information.");
         if (game_start) {
-            mpr("See http://crawl.montres.org.uk/about-dev.html for changes.\nRecent change: Veh gifts do not expire; misc vanilla Veh tweaks/fixes.");
+            mpr("See http://crawl.montres.org.uk/about-dev.html for changes.\nRecent change: rot heals with XP gain (too fast? too slow?).");
         }
     }
 
