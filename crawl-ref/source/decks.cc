244240ca96dd9547a65c079d9375518d18cb9378
David Damerell <damerell@chiark.greenend.org.uk>
2022-06-19 12:05:04 +0100

    Foxfire card can give bumblebees again

--- crawl-ref/source/decks.cc
+++ crawl-ref/source/decks.cc
@@ -1721,8 +1721,8 @@ static void _summon_flying(int power, deck_rarity_type rarity)
 
     const monster_type flytypes[] =
     {
-        MONS_INSUBSTANTIAL_WISP, MONS_WYVERN, MONS_KILLER_BEE, MONS_FIREFLY,
-        MONS_VAMPIRE_MOSQUITO, MONS_HORNET
+        MONS_BUTTERFLY, MONS_INSUBSTANTIAL_WISP, MONS_WYVERN, MONS_KILLER_BEE,
+        MONS_BUMBLEBEE, MONS_FIREFLY, MONS_VAMPIRE_MOSQUITO, MONS_HORNET
     };
     const int num_flytypes = ARRAYSZ(flytypes);
 
