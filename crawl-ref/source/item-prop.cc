bc06145bf5f8b09bbf07af0fa6478ee6b65d57e8
David Damerell <damerell@chiark.greenend.org.uk>
2024-05-24 13:49:44 +0100

    Revert "Remove code for using decks when worshiping gods other than Nemelex"
    This reverts commit bb4e2712d2eefb8fa8e663b9b3f73f7faeeb2c83.
    
    But TSO no longer minds poison
    
    Change Nemelex descriptions for decks-for-all world
    
    Revert "Fix Nemelex penance description"
    
    This reverts commit 64cbba572c6cfc18bda3cb221101a437a51e97fe.
    
    Revert "Fix colouring for Nemelex wrath on ^O"
    
    This reverts commit 5f97612e966a469efafe64d1ae4fd4b6e10e8e4a.
    
    Revert "Remove Nemelex evocations reference (10847)"
    
    This reverts commit ec7c366be5b35eccc1ea00bcbb1e2ae0bee8a360.
    
    Revert "Make Nemelex wrath reclaim all decks"
    
    This reverts commit 358a8cc3fbf1c18dc7e8a6822872691420df3306.
    
    Revert "Remove obsolete Nemelex/sac artifice handling"
    
    This reverts commit 962fb7dd74fe97b3a2471dd2691fce602a4a9730.
    
    Documentation update for new old Nemelex
    
    Revert "Fix deck interactions with sacrifice artifice and evo skill"
    
    This reverts commit d1aef3372b1d537b8dc741c9ca01c7c1e7f9499e.
    
    Revert "Refactor some deck handling"
    
    This reverts commit aaa67c4bcbe2213d4ec638df255ac811d2cbd145.

--- crawl-ref/source/item-prop.cc
+++ crawl-ref/source/item-prop.cc
@@ -2079,7 +2079,6 @@ bool item_skills(const item_def &item, set<skill_type> &skills)
     // Jewellery with evokable abilities, wands and similar unwielded
     // evokers allow training.
     if (item_is_evokable(item, false, false, false, true)
-        && !is_deck(item)
         || item.base_type == OBJ_JEWELLERY && gives_ability(item))
     {
         skills.insert(SK_EVOCATIONS);
@@ -2101,7 +2100,6 @@ bool item_skills(const item_def &item, set<skill_type> &skills)
         return !skills.empty();
 
     if (item_is_evokable(item, false, false, false, false)
-        && !is_deck(item)
         || staff_uses_evocations(item)
         || item.base_type == OBJ_WEAPONS && gives_ability(item))
     {
--- crawl-ref/source/tags.cc
+++ crawl-ref/source/tags.cc
@@ -36,7 +36,6 @@
 #include "butcher.h"
 #if TAG_MAJOR_VERSION == 34
  #include "cloud.h"
- #include "decks.h"
 #endif
 #include "colour.h"
 #include "coordit.h"
@@ -3368,14 +3367,6 @@ static void tag_read_you(reader &th)
         }
 #endif
         you.penance[i] = unmarshallUByte(th);
-#if TAG_MAJOR_VERSION == 34
-        if (th.getMinorVersion() < TAG_MINOR_NEMELEX_WRATH
-            && player_under_penance(GOD_NEMELEX_XOBEH)
-            && i == GOD_NEMELEX_XOBEH)
-        {
-            you.penance[i] = max(you.penance[i] - 100, 0);
-        }
-#endif
         ASSERT(you.penance[i] <= MAX_PENANCE);
     }
 
@@ -4046,15 +4037,6 @@ static void tag_read_you_items(reader &th)
                                                            | (seed3 << 16);
         }
     }
-    // Remove any decks if no longer worshipping Nemelex, now that items have
-    // been loaded.
-    if (th.getMinorVersion() < TAG_MINOR_NEMELEX_WRATH
-        && !you_worship(GOD_NEMELEX_XOBEH)
-        && you.num_total_gifts[GOD_NEMELEX_XOBEH])
-    {
-        nemelex_reclaim_decks();
-    }
-
     // Reset training arrays for transfered gnolls that didn't train all skills.
     if (th.getMinorVersion() < TAG_MINOR_GNOLLS_REDUX)
         reset_training();
