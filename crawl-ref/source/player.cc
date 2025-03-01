2137588d0a01594196440fe921c567eda7a3c89f
DracoOmega <draco.omega.dcss.dev@gmail.com>
2025-03-01 05:06:02 -0330

    Datify transformation holiness, make players real plants
    Rather than being contained inside player::holiness(), forms can now
    specify holiness changes directly in data.
    
    (Vampire and bat swarm are still special-cased, since they apply undead
    holiness on top of the player's default holiness, rather than replacing it
    entirely.)
    
    This commit also makes fungus and tree form actually give the player plant
    holiness.
    
    Previously, the player was given most of the benefits of having plant
    holiness in an ad hoc fashion that checked each form directly and wasn't
    even always consistent (for instance, fungus form wasn't unbreathing, but
    tree form was!).
    
    There may be some minimal functional change caused by this, but nothing
    significant that I have noticed (besides the aforementioned bug).

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -6489,7 +6489,6 @@ mon_holy_type player::holiness(bool temp, bool incl_form) const
 {
     mon_holy_type holi;
 
-    // Forms take precedence over a species' base holiness
     if (species::undead_type(species) == US_UNDEAD)
         holi = MH_UNDEAD;
     else if (species::is_nonliving(you.species))
@@ -6497,26 +6496,19 @@ mon_holy_type player::holiness(bool temp, bool incl_form) const
     else
         holi = MH_NATURAL;
 
+    // Forms take precedence over a species' base holiness
     if (incl_form)
     {
-        // TODO: move this into data
         const transformation f = temp ? form : default_form;
-        if (f == transformation::statue
-             || f == transformation::wisp
-             || f == transformation::storm)
-        {
-            holi = MH_NONLIVING;
-        }
-        else if (f == transformation::slaughter)
-            holi = MH_DEMONIC;
-        else if (f == transformation::death)
-            holi = MH_UNDEAD;
-        // Both living and undead weaknesses
-        else if (f == transformation::vampire
+        // Special-cased to add undead holiness onto the player's basse type,
+        // rather than replace it
+        if (f == transformation::vampire
                  || f == transformation::bat_swarm)
         {
             holi |= MH_UNDEAD;
         }
+        else if (get_form(f)->holiness != MH_NONE)
+            holi = get_form(f)->holiness;
     }
 
     // Petrification takes precedence over base holiness and lich form
@@ -6568,7 +6560,7 @@ int player::how_chaotic(bool /*check_spells_god*/) const
 bool player::is_unbreathing() const
 {
     return is_nonliving() || is_lifeless_undead()
-           || form == transformation::tree;
+           || bool(holiness() & MH_PLANT);
 }
 
 bool player::is_insubstantial() const
@@ -6676,13 +6668,7 @@ bool player::res_torment() const
 
     return get_form()->res_neg() == 3
            || you.petrified()
-    // This should probably be (you.holiness & MH_PLANT), but
-    // transforming doesn't currently make you a plant, and I suspect
-    // changing that would cause other bugs. (For example, being able
-    // to wield holy weapons as a demonspawn in treeform & keep them
-    // while untransformed?)
-           || you.form == transformation::tree
-           || you.form == transformation::fungus
+           || bool(you.holiness() & MH_PLANT)
 #if TAG_MAJOR_VERSION == 34
            || you.unrand_equipped(UNRAND_ETERNAL_TORMENT)
 #endif
@@ -7699,9 +7685,7 @@ bool player::can_smell() const
 bool player::can_sleep(bool holi_only) const
 {
     return !you.duration[DUR_STUN_IMMUNITY]
-           && actor::can_sleep(holi_only)
-           && !(you.form == transformation::fungus)
-           && !(you.form == transformation::tree);
+           && actor::can_sleep(holi_only);
 }
 
 /**
