4dbf5de2fc6be4c21a336f71486e454876bb7922
David Damerell <damerell@chiark.greenend.org.uk>
2026-08-04 15:25:23 +0100

    Report on health of healable allies when they are hit/healed
    ie inugami and the Hep ancestor

--- crawl-ref/source/spl-summoning.cc
+++ crawl-ref/source/spl-summoning.cc
@@ -333,6 +333,7 @@ spret cast_call_canine_familiar(int pow, god_type god, bool fail)
         old_dog->heal(random_range(5, 9) + div_rand_round(pow, 5));
         old_dog->lose_ench_levels(ENCH_POISON, 1);
         old_dog->add_ench(mon_enchant(ENCH_INSTANT_CLEAVE, 1, &you, 50));
+        print_wounds(*old_dog);
 
         // Give our familiar a small amount of extra duration, if its duration
         // is currently low, to avoid imbuing it and then having it immediately
