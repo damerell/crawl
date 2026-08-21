f1a1d793b85506f834ef2ef07fe040c17e5d3a56
David Damerell <damerell@chiark.greenend.org.uk>
2026-08-22 00:19:25 +0100

    tool/tile: Don't use memset() and memcpy() on array of tile_color (niklata)
    tile_color has a constructor so this isn't valid.
    
    Fix memset() by simply providing a default constructor that
    zero-initializes.
    
    Fix memcpy() by hand-writing a copy loop that uses the copy
    assignment operator.

--- crawl-ref/source/rltiles/tool/tile_colour.h
+++ crawl-ref/source/rltiles/tool/tile_colour.h
@@ -28,7 +28,7 @@ enum COLORS
 class tile_colour
 {
 public:
-    tile_colour() {};
+    tile_colour() : r(0), g(0), b(0), a(0) {};
     tile_colour(unsigned char _r, unsigned char _g, unsigned char _b,
         unsigned char _a) : r(_r), g(_g), b(_b), a(_a) {}
 
